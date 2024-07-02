f36b8ff4dd97abef82539b5c60ce8cf2cf53633e
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-07-02 17:05:01 -0400

    Registering user in user table

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,15 +1,23 @@
-const AppError = require('../utils/AppError')
+const knex = require("../database/knex");
+const { hash } = require("bcryptjs");
+const AppError = require('../utils/AppError');
 
 class UsersController {
-  create(request, response) {
-    const { name, email, password } = request.body
+  async create(request, response) {
+    const { name, email, password } = request.body;
 
-    if (!name) {
-      throw new AppError('Nome é obrigatório')
+    const checkUserExists = await knex("users").where({ email });
+
+    if (checkUserExists.length > 0) {
+      throw new AppError("Este e-mail já está em uso.");
     }
 
-    response.status(201).json({ name, email, password })
+    const hashedPassword = await hash(password, 8);
+
+    await knex("users").insert({ name, email, password: hashedPassword });
+
+    return response.status(201).json();
   }
 }
 
-module.exports = UsersController
+module.exports = UsersController;
