91c0247bf8c8d6d3b50cf3a37f310e45d9c6dff7
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-07-02 16:58:05 -0400

    Setting knex query builder instead of using sqlite and create table users

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require('express-async-errors');
-const database = require('./database/sqlite');
+
 const AppError = require('./utils/AppError');
 
 const express = require('express');
@@ -10,8 +10,6 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
-
-database();
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) { 
