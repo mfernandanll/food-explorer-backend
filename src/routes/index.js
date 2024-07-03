ea82d7625cd75acc0fe99c7ae2e84daa00554d59
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-07-03 13:28:59 -0400

    Adding authentication routes and controller and defining some validating rules

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const { Router } = require('express');
 
 const usersRoutes = require('./users.routes');
 const dishesRoutes = require("./dishes.routes");
+const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
 routes.use('/users', usersRoutes);
 routes.use("/dishes", dishesRoutes);
+routes.use("/sessions", sessionsRouter);
 
 module.exports = routes;
